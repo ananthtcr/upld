2693a6f1e15a846dea67e6036b1008adc8865acb
Ray Nicholus <rnicholus@widen.com>
2012-08-23 22:30:27 -0500

    uploads should work on gh-pages

--- js/uploader-demo.js
+++ js/uploader-demo.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     var uploader = new qq.FileUploader({
         element: $('#basicUploadSuccessExample')[0],
-        action: "success.html"
+        action: "success.html",
+        demoMode: true
     });
 
 
@@ -9,7 +10,8 @@ $(document).ready(function() {
     var uploader2 = new qq.FileUploader({
         element: $('#manualUploadModeExample')[0],
         action: "success.html",
-        autoUpload: false
+        autoUpload: false,
+        demoMode: true
     });
 
     $('#triggerUpload').click(function() {
@@ -19,6 +21,7 @@ $(document).ready(function() {
 
     var uploader3 = new qq.FileUploader({
         element: $('#basicUploadFailureExample')[0],
-        action: "failure.html"
+        action: "failure.html",
+        demoMode: true
     });
 });
