c15fcee006063653bb29c47789584702a5796ea4
Ray Nicholus <github.com@garstasio.com>
2013-03-31 21:51:22 -0500

    update (simplify) limit files demos by using new itemLimit validation option

--- js/demo/limit-files.js
+++ js/demo/limit-files.js
@@ -1,8 +1,3 @@
-var manualuploader;
-var fileCount = 0;
-var addedFiles = 0;
-var fileLimit = 3;
-
 var filelimituploader = new qq.FineUploader({
     element: $('#filelimit-fine-uploader')[0],
     request: {
@@ -10,32 +5,8 @@ var filelimituploader = new qq.FineUploader({
     },
     validation: {
         allowedExtensions: ['jpeg', 'jpg', 'gif', 'png'],
-        sizeLimit: 51200 // 50 kB = 50 * 1024 bytes
-    },
-    callbacks: {
-        onSubmit: function(id, fileName) {
-            fileCount ++;
-            if(fileCount > fileLimit) {
-                $('#filelimit-fine-uploader .qq-upload-button').hide();
-                $('#filelimit-fine-uploader .qq-upload-drop-area').hide();
-                return false;
-            }
-        },
-        onCancel: function(id, fileName) {
-            fileCount --;
-            if(fileCount <= fileLimit) {
-                $('#filelimit-fine-uploader .qq-upload-button').show();
-            }
-        },
-        onComplete: function(id, fileName, responseJSON) {
-            if (responseJSON.success) {
-                addedFiles ++;
-                if(addedFiles >= fileLimit) {
-                    $('#filelimit-fine-uploader .qq-upload-button').hide();
-                    $('#filelimit-fine-uploader .qq-upload-drop-area').hide();
-                }
-            }
-        }
+        sizeLimit: 51200, // 50 kB = 50 * 1024 bytes
+        itemLimit: 3
     },
     debug: true,
     demoMode: true // Undocumented -> Only for the gh-pages demo website
