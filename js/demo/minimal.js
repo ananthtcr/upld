c49841c408a34b1e1e80d68a46fffa6eccff5c77
Mark Feltner <feltner.mj@gmail.com>
2013-09-27 10:44:05 -0500

    Initial commit

--- js/demo/minimal.js
+++
@@ -1,9 +0,0 @@
-var uploader = new qq.FineUploader({
-    multiple: !qq.ios7(),
-    element: document.getElementById('fine-uploader'),
-    request: {
-        endpoint: 'server/success.html'
-    },
-    debug: true,
-    demoMode: true // Undocumented -> Only for the gh-pages demo website
-});
