49a7e888e7c2fb4bcd48fa396442bb29d89cc19f
Ray Nicholus <rnicholus@widen.com>
2013-10-11 14:09:56 -0500

    feat(*): #868, #869 - update thumbnail/preview demos
    [skip ci]

--- js/demo/display-thumbnails.js
+++ js/demo/display-thumbnails.js
@@ -1,18 +1,12 @@
 var thumbnailuploader = new qq.FineUploader({
     multiple: !qq.ios7(),
-    element: $('#thumbnail-fine-uploader')[0],
+    template: "qq-simple-thumbnails-template",
+    element: document.getElementById("thumbnail-fine-uploader"),
     request: {
         endpoint: 'server/success.html'
     },
     validation: {
         allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
-    },
-    callbacks: {
-        onComplete: function(id, fileName, responseJSON) {
-            if (responseJSON.success) {
-                $('#thumbnail-fine-uploader').append('<img src="img/success.jpg" alt="' + fileName + '">');
-            }
-        }
     },
     debug: true,
     demoMode: true // Undocumented -> Only for the gh-pages demo website
--- js/demo/jquery-display-thumbnails.js
+++ js/demo/jquery-display-thumbnails.js
@@ -1,18 +1,12 @@
 $('#thumbnail-fine-uploader').fineUploader({
     multiple: !qq.ios7(),
+    template: "qq-simple-thumbnails-template",
     request: {
         endpoint: 'server/success.html'
     },
     validation: {
         allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
     },
-    text: {
-        uploadButton: 'Click or Drop'
-    },
     debug: true,
     demoMode: true // Undocumented -> Only for the gh-pages demo website
-}).on('complete', function(event, id, fileName, responseJSON) {
-    if (responseJSON.success) {
-        $(this).append('<img src="img/success.jpg" alt="' + fileName + '">');
-    }
 });
