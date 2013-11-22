6b3122344b0cc07b25a54e21c202b7477ec4b49f
Joshua Ard <jard@widen.com>
2013-11-22 11:17:29 -0600

    starting to implement parallax effects

--- js/ui/ui-scripts.js
+++ js/ui/ui-scripts.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-    console.log('ready!');
-    var num = 85; //number of pixels before modifying styles
-
-    $(window).bind('scroll', function () {
-        if ($(window).scrollTop() > num) {
-            $('.jumbotron-small').addClass('fixed');
-                console.log('it is fixed');
-                $('body').css("padding-top", "71px");
-
-        } else {
-            $('.jumbotron-small').removeClass('fixed');
-            $('body').css({'padding-top' : '30px',
-                           'height' : 'auto'
-            });
-        }
-    });
-});
-
+//$(document).ready(function() {
+//    console.log('ready!');
+//    var num = 85; //number of pixels before modifying styles
+//
+//    $(window).bind('scroll', function () {
+//        if ($(window).scrollTop() > num) {
+//            $('.jumbotron-small').addClass('fixed');
+//                console.log('it is fixed');
+//                $('body').css("padding-top", "71px");
+//
+//        } else {
+//            $('.jumbotron-small').removeClass('fixed');
+//            $('body').css({'padding-top' : '30px',
+//                           'height' : 'auto'
+//            });
+//        }
+//    });
+//});
+//
