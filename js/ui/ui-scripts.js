c2cc68af15cb3b3f3c2f625bbdf2d8a00c491711
Joshua Ard <jard@widen.com>
2013-12-04 14:15:53 -0600

    styling applied across all pages

--- js/ui/ui-scripts.js
+++ js/ui/ui-scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 //    console.log('ready!');
-//    var num = 105; //number of pixels before modifying styles
+//    var num = 275; //number of pixels before modifying styles
 //
 //    $(window).bind('scroll', function () {
 //        if ($(window).scrollTop() > num) {
-//            $('.demo-nav').addClass('fixed');
+//            $('.demo-subnav').addClass('fixed');
 //                console.log('it is fixed');
 ////                $('body').css("padding-top", "40px");
-//                $('.demo-nav').css("margin-top", "40px");
+//                $('.demo-subnav').css("margin-top", "40px");
 //
 //
 //        } else {
-//            $('.demo-nav').removeClass('fixed');
+//            $('.demo-subnav').removeClass('fixed');
 ////            $('body').css({'padding-top' : '30px',
 ////                           'height' : 'auto'
 ////            });
-//            $('.demo-nav').css("margin-top", "0px");
+//            $('.demo-subnav').css("margin-top", "0px");
 //        }
 //    });
     $( '.view-code a' ).click(function() {
