f60dea3bfc9ec60c95043b33ea497a87db6da59d
Mark Feltner <mfeltner@widen.com>
2014-12-22 19:59:55 -0600

    refactor(customize.html): out with the builder
    [skip ci]
    closes #1339
    
    refactor(customize.html): features are lists with links
    
    refactor(customize.html): use radio buttons for server selector
    
    style(customize.html): ensure jquery checkbox looks consistent
    
    refactor(customize.html): remove licensee email field
    
    refactor(customize.html): form action is null and move jquery option below server
    
    feat(customize.html/js): add js to generate s3 url
    
    feat(customize.js): add hidden iframe to trigger file downloads

---
+++ js/customize.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    var BASE_URL = 'http://s3.amazonaws.com',
+        BUCKET = 'releases.fineuploader.com',
+        VERSION = '5.1.0-12';
+
+    $("#customize-form").submit(function(e) {
+
+        var jquery = '',
+            endpoint = '',
+            base = 'fineuploader-' + VERSION + '.zip';
+
+        $("#jquery_fieldset").find("input[type='checkbox']:checked").each(function(i, checkbox){
+            jquery = 'jquery.';
+        });
+
+        $("#ep_fieldset").find("input[type='radio']:checked").each(function(i, radio){
+            if (radio.value) {
+                endpoint = radio.value + '.';
+            }
+        });
+
+
+        var url = BASE_URL + '/' + BUCKET + '/' + VERSION + '/' + endpoint + jquery + base;
+        $("<iframe>")
+            .hide()
+            .prop("src", url)
+            .appendTo("body");
+
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    });
+});
