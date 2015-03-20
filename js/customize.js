81044b6bfac0a0d1fb01a4647743addfa20fb9fa
Ray Nicholus <rnicholus@users.noreply.github.com>
2015-03-20 16:06:59 -0500

    chore(site): prepare for 5.2.0 release [skip ci]

--- js/customize.js
+++ js/customize.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var BASE_URL = 'http://releases.fineuploader.com',
-        version = '5.1.3';
+        version = '5.2.0';
 
         updateDownloadUrl = function() {
             var base = 'fine-uploader-' + version + '.zip',
