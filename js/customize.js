ff537e83d0c0c35f765c715cae904ab343da59ec
Ray Nicholus <rnicholus@users.noreply.github.com>
2015-09-22 11:44:10 -0500

    allow 5.3.2 to be downloaded

--- js/customize.js
+++ js/customize.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var BASE_URL = 'http://releases.fineuploader.com',
-        version = '5.3.0';
+        version = '5.3.2';
 
         updateDownloadUrl = function() {
             var base = 'fine-uploader-' + version + '.zip',
