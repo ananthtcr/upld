9b8a7dc74415b1dfe6159c0bce75bc6c90bc3be5
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-01-26 15:05:41 +0700

    feat(ie7): eliminate qq.parseJson #1344 #637

--- client/js/traditional/traditional.xhr.upload.handler.js
+++ client/js/traditional/traditional.xhr.upload.handler.js
@@ -100,7 +100,7 @@ qq.traditional.XhrUploadHandler = function(spec, proxy) {
 
             try {
                 log(qq.format("Received response status {} with body: {}", xhr.status, xhr.responseText));
-                response = qq.parseJson(xhr.responseText);
+                response = JSON.parse(xhr.responseText);
             }
             catch (error) {
                 upload && log("Error when attempting to parse xhr response text (" + error.message + ")", "error");
