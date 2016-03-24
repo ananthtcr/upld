c544ff7fe673db99837f513865d11abd04eae74b
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-03-24 14:30:22 -0500

    chore(release): Allow 5.6.0 to be downloaded

--- js/customize.js
+++ js/customize.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var BASE_URL = 'http://releases.fineuploader.com',
-        version = '5.5.2';
+        version = '5.6.0';
 
         updateDownloadUrl = function() {
             var base = 'fine-uploader-' + version + '.zip',
