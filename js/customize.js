e04832ba34a7970971a375981ae38f8615f3d72a
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-06-15 17:32:54 -0500

    chore(site): make 5.10.0 available for download on fineuploader.com #1576

--- js/customize.js
+++ js/customize.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var BASE_URL = 'http://releases.fineuploader.com',
-        version = '5.9.0',
+        version = '5.10.0',
 
         updateDownloadUrl = function() {
             var base = 'fine-uploader-' + version + '.zip',
