67c4c085ca345e9b3eccbca10035d22330281d8f
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-07-11 09:27:50 -0500

    site(download): allow 5.10.1 to be downloaded [skip ci]

--- js/customize.js
+++ js/customize.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var BASE_URL = 'http://releases.fineuploader.com',
-        version = '5.10.0',
+        version = '5.10.1',
 
         updateDownloadUrl = function() {
             var base = 'fine-uploader-' + version + '.zip',
